6e601df78fa92005acc4c8764bac466165e99bd9
Zeliwipin <zeliwipin@gmail.com>
2022-02-23 18:58:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Editor } from "./components/Editor/Editor";
 
 function App() {
+  const [html, setHtml] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      <html>
+      <body>${html}</body>
+      <script src="https://cdn.tailwindcss.com"></script>
+      </html>`);
+    }, 250);
+    return () => clearTimeout(timeout);
+  }, [html]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="top-pannel">
+        <Editor
+          displayName="HTML"
+          language="xml"
+          value={html}
+          onChange={setHtml}
+        />
+      </div>
+      <div className="bottom-pannel">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+          srcDoc={srcDoc}
+        />
+      </div>
+    </>
   );
 }
 
